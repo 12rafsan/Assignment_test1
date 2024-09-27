3950281d1cd9cbba7b1f1e8cb1c1fd8a19486708
MD.RAFSAN ZANI <rafsanzanirizon539@gmail.com>
2024-09-27 23:10:05 +0600

    done

--- dart.dart
+++ dart.dart
@@ -1,21 +1,25 @@
-import 'dart:io';
+class Car {
+  String brand;
+  String model;
+  int year;
 
-void main() {
+  Car(this.brand, this.model, this.year);
 
-  stdout.write("Enter the temperature in Celsius: ");
-  String? input = stdin.readLineSync();
-  if (input != null) {
-    try {
-      double celsius = double.parse(input);
-      double fahrenheit = (celsius * 9 / 5) + 32;
-      print("Temperature in Fahrenheit: ${fahrenheit.toStringAsFixed(2)}");
-    } catch (e) {
-      print("Please enter a valid number.");
-    }
-  } else {
-    print("No input received.");
+  int carAge() {
+    int currentYear = DateTime.now().year;
+    return currentYear - year;
   }
 }
+
+void main() {
+  Car myCar = Car('Toyota', 'Corolla', 2015);
+
+  print('Brand: ${myCar.brand}');
+  print('Model: ${myCar.model}');
+  print('Year: ${myCar.year}');
+  print('Car Age: ${myCar.carAge()} years');
+}
+
 
 
 
